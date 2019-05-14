211a159af30159f757b78b94299d6cd2c772a9a1
zwy <zhou_wenya@163.com>
2019-05-14 18:15:02 +0800

    初始化用户服务

---
+++ userservice/src/main/java/com/renyijia/UserServiceApplication.java
@@ -0,0 +1,19 @@
+package com.renyijia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author : zhouwenya
+ * @version : 1.0
+ * @date : 2019-05-14
+ * @email : zhou_wenya@163.com
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class,args);
+    }
+}
