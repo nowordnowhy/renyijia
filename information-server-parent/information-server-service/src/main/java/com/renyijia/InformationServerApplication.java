0b644c94c5ad25733e46e459cc225c6051c89586
zwy <zhou_wenya@163.com>
2019-05-15 16:13:53 +0800

    加入客户端负载均衡和熔断

--- information-server-parent/information-server-service/src/main/java/com/renyijia/InformationServerApplication.java
+++ information-server-parent/information-server-service/src/main/java/com/renyijia/InformationServerApplication.java
@@ -2,7 +2,10 @@ package com.renyijia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author : zhouwenya
@@ -13,6 +16,13 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @SpringBootApplication
 public class InformationServerApplication {
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(InformationServerApplication.class, args);
     }
